0f1720d8e32986d3e14f2a45a1731843d09783b3
Lemenet <lemenet@live.com>
2017-10-03 22:56:54 +0800

    2017/10/03 创建工程，并添加了二叉树的数据结构
    创建了两个工程，一个库文件工程，一个测试工程
    添加了二叉树的基本数据结构。

---
+++ HMD_Library/DataStructure/Private/BinaryTree.cpp
@@ -0,0 +1,12 @@
+
+#include "BinaryTree.h"
+using namespace HMDL;
+
+
+inline BinaryTreeNode::BinaryTreeNode()
+{
+	Parent = nullptr;
+	LeftChild = nullptr;
+	RightCild = nullptr;
+	Data = nullptr;
+}
